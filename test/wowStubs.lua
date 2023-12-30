8241d4d66f4d2f9b8c4529f81e4364f643b8fc46
opussf <github@zz9-za.com>
2023-12-29 23:55:45 -0800

    And Stubs update
    Speed is subsittute fo accurancy.

--- test/wowStubs.lua
+++ test/wowStubs.lua
@@ -1,7 +1,7 @@
 -----------------------------------------
 -- Author  :  Opussf
--- Date    :  May 20 2023
--- Revision:  9.0.4-4-g4dbc6c4
+-- Date    :  December 29 2023
+-- Revision:  9.0.4-9-gcdffaac
 -----------------------------------------
 -- These are functions from wow that have been needed by addons so far
 -- Not a complete list of the functions.
@@ -69,6 +69,7 @@ SlotListMap={ "HeadSlot","NeckSlot","ShoulderSlot","ShirtSlot","ChestSlot","Wais
 }
 myGear = {} -- items that are equipped in the above slots, index matching
 Items = {
+	["6948"] = {["name"] = "Hearthstone", ["link"] = "|cffffffff|Hitem:6948::::::::70:258:::::::::|h[Hearthstone]|h|r", ["texture"] = ""},
 	["7073"] = {["name"] = "Broken Fang", ["link"] = "|cff9d9d9d|Hitem:7073:0:0:0:0:0:0:0:80:0:0|h[Broken Fang]|h|r", ["texture"] = ""},
 	["6742"] = {["name"] = "UnBroken Fang", ["link"] = "|cff9d9d9d|Hitem:6742:0:0:0:0:0:0:0:80:0:0|h[UnBroken Fang]|h|r", ["texture"] = ""},
 	["22261"] = {["name"] = "Love Fool", ["link"] = "|cff9d9d9d|Hitem:22261:0:0:0:0:0:0:0:80:0:0|h[Love Fool]|h|r", ["texture"] = ""},
@@ -394,11 +395,13 @@ Frame = {
 		["SetHeight"] = function(self, value) self.height = value; end,
 		["GetHeight"] = function(self) return( self.height ); end,
 		["CreateFontString"] = function(self, ...) return(CreateFontString(...)) end,
+		["SetSize"] = function(self, x, y) end,
+		["ClearAllPoints"] = function(self) end,
 
 		["SetMinMaxValues"] = function(self, min, max) self.min=min; self.max=max; end,
 		["SetValue"] = function(self, value) self.value=value end,
 		["SetStatusBarColor"] = function() end,
-		["SetScript"] = function() end,
+		["SetScript"] = function(self, event, func) end,
 		["SetAttribute"] = function() end,
 
 		["SetChecked"] = function() end,
@@ -1664,17 +1667,54 @@ TooltipDataProcessor = {}
 function TooltipDataProcessor.AddTooltipPostCall()
 end
 
+----------
+-- Macros
+----------
+myMacros = {}  -- ["macroName"] = { ["icon"] = "", ["text"] = "", ["isLocal"] = true }
+function GetMacroInfo( macroName )
+	-- returns:  macroName, macroIcon, macroText, isLocal (bool)
+	if myMacros[macroName] then
+		return macroName, myMacros[macroName][icon], myMacros[macroName][text], myMacros[macroName][isLocal] or false
+	end
+end
+function CreateMacro( macroName, macroIcon, macroText, perChar )
+	-- returns: macroID
+	if macroName and not myMacros[macroName] then
+		myMacros[macroName] = { ["icon"] = macroIcon, ["text"] = macroText }
+		return 1 -- return the ID
+	end
+end
+function EditMacto( macroName, newName, newIcon, body, islocal, perChar )
+	-- returns: new macroID
+	if macroName and myMacros[macroName] then
+		if newName then
+			myMacros[newName] = myMacros[macroName]
+			macroName = newName
+		end
+		if newIcon then
+			myMacros[macroName].icon = newIcon
+		end
+		myMacros[macroName].text = body
+		return 1 -- return the ID
+	end
+end
+
+--------
+-- C_ChatInfo
+--------
 C_ChatInfo = {}
-function C_ChatInfo.IsAddonMessagePrefixRegistered()
+function C_ChatInfo.IsAddonMessagePrefixRegistered( prefix )
 	return true
+end
+function C_ChatInfo.RegisterAddonMessagePrefix( prefix )
 end
 function C_ChatInfo.SendAddonMessage()
 	return true
 end
+
 -----------------------------------------
 -- XML functions
 function ParseXML( xmlFile )
-
 end
 
 -----------------------------------------

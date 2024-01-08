6ddc9dbd08e6f2c4a6003dfc6310c8010ecef192
opussf <github@zz9-za.com>
2024-01-08 02:58:56 -0800

    Back to original, with +
    If you look in the right places, you can find some good offerings.

--- src/Steps.lua
+++ src/Steps.lua
@@ -331,8 +331,8 @@ function STEPS.TooltipSetUnit( arg1, arg2 )
 	end
 	if name and Steps_data[realm] and Steps_data[realm][name] then
 		for dayBack=-1,1 do
-			local dateStr = date("%Y%m%d", time() - (dayBack*86400))
-			if not today and Steps_data[realm][name][dateStr] then
+			local dateStr = date("%Y%m%d", time() + (dayBack*86400))
+			if Steps_data[realm][name][dateStr] then
 				today = Steps_data[realm][name][dateStr].steps
 			end
 		end

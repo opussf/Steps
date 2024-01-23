b7a175522ad0ee590c2cdef211447af754834d8a
opussf <github@zz9-za.com>
2024-01-22 20:21:46 -0800

    Options command and initial panel
    Life is a symphony – and you're playing the kazoo.

--- src/StepsOptions.lua
+++ src/StepsOptions.lua
@@ -1,1 +1,21 @@
 -- STEPSOptions @VERSION@
+
+
+function STEPS.OptionsPanel_OnLoad(panel)
+	panel.name = "Steps"
+	-- INEEDOptionsFrame_Title:SetText(INEED_MSG_ADDONNAME.." "..INEED_MSG_VERSION)
+	-- --panel.parent=""
+	-- panel.okay = INEED.OptionsPanel_OKAY
+	-- panel.cancel = INEED.OptionsPanel_Cancel
+	-- --panel.default = FB.OptionsPanel_Default;
+	-- panel.refresh = INEED.OptionsPanel_Refresh
+
+	-- InterfaceOptions_AddCategory(panel)
+	-- --InterfaceAddOnsList_Update();
+	-- --FB.OptionsPanel_TrackPeriodSlider_OnLoad()
+end
+
+Steps.commantList["options"] = {
+	["func"] = function() InterfaceOptionsFrame_OpenToCategory( STEPS_MSG_ADDONNAME ) end,
+	["help"] = {"", "Open the options panel"},
+}

8e07a43cd13803ed23510f965813b9fb13d549be
opussf <github@zz9-za.com>
2024-08-03 17:01:04 -0700

    DeNormalize some realm names...
    Focus on your long-term goal. Success is soon yours!

--- test/wowTest.lua
+++ test/wowTest.lua
@@ -1,7 +1,7 @@
 -----------------------------------------
 -- Author  :  Opussf
--- Date    :  May 15 2024
--- Revision:  9.4.3
+-- Date    :  August 2 2024
+-- Revision:  9.4.3-1-g220d744
 -----------------------------------------
 -- This is an uber simple unit test implementation
 -- It creates a dictionary called test.

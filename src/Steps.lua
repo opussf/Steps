ed3a530971074bc97ae7aca9cd90b17ef1d64e3f
opussf <github@zz9-za.com>
2024-11-15 18:36:46 -0800

    Re-enable DrownDowns, add options panel
    Men do not fail... they give up trying.

--- src/Steps.lua
+++ src/Steps.lua
@@ -142,9 +142,7 @@ function Steps.BuildAddonMessage2()
 					select(2, Steps.toBytes( tonumber( dayStr ) ) ),
 					select(2, Steps.toBytes( math.ceil( Steps.mine[dayStr].steps ) ) )
 			)
-			print( string.len( msgStr ).."+"..string.len( daySteps)..">=? 255" )
 			if ( prefixLen + string.len( msgStr ) + string.len( daySteps ) + 1 >= 255 ) then
-				print( "Broke: "..(prefixLen + string.len( msgStr ) + string.len( daySteps ) + 1).." >= 255" )
 				break
 			end
 			msgStr = msgStr .. "|" .. daySteps
@@ -444,17 +442,17 @@ function Steps.TooltipSetUnit( arg1, arg2 )
 	end
 end
 -- DropDownMenu
--- function Steps.ModifyMenu( owner, rootDescription, contextData )
--- 	today, total = Steps.GetTodayTotal( contextData.name, (contextData.server and Steps.DeNormalizeRealm( contextData.server ) or GetRealmName()) )
--- 	if today then
--- 		rootDescription:CreateDivider()
--- 		rootDescription:CreateTitle("Steps today: "..today.." total: "..total)
--- 	end
--- end
--- Menu.ModifyMenu("MENU_UNIT_SELF", Steps.ModifyMenu)
--- Menu.ModifyMenu("MENU_UNIT_COMMUNITIES_GUILD_MEMBER", Steps.ModifyMenu)
--- Menu.ModifyMenu("MENU_UNIT_PARTY", Steps.ModifyMenu)
--- Menu.ModifyMenu("MENU_UNIT_RAID", Steps.ModifyMenu)
+function Steps.ModifyMenu( owner, rootDescription, contextData )
+	today, total = Steps.GetTodayTotal( contextData.name, (contextData.server and Steps.DeNormalizeRealm( contextData.server ) or GetRealmName()) )
+	if today then
+		rootDescription:CreateDivider()
+		rootDescription:CreateTitle("Steps today: "..today.." total: "..total)
+	end
+end
+Menu.ModifyMenu("MENU_UNIT_SELF", Steps.ModifyMenu)
+Menu.ModifyMenu("MENU_UNIT_COMMUNITIES_GUILD_MEMBER", Steps.ModifyMenu)
+Menu.ModifyMenu("MENU_UNIT_PARTY", Steps.ModifyMenu)
+Menu.ModifyMenu("MENU_UNIT_RAID", Steps.ModifyMenu)
 -- Post
 function Steps.GetPostString()
 	local dateStr = date("%Y%m%d")
---
+++ src/StepsOptions.lua
@@ -0,0 +1,50 @@
+-- StepsOptions @VERSION@
+function Steps.OptionsPanel_OnLoad( panel )
+	panel.name = "Steps"
+	StepsOptionsFrame_Title:SetText(Steps_MSG_ADDONNAME.." v"..Steps_MSG_VERSION)
+
+	-- These NEED to be set
+	panel.OnDefault = function() end
+	panel.OnRefresh = function() end
+	panel.OnCommit = Steps.OptionsPanel_OKAY
+	panel.cancel = Steps.OptionsPanel_Cancel
+
+	local category, layout = Settings.RegisterCanvasLayoutCategory( panel, panel.name )
+	panel.category = category
+	Settings.RegisterAddOnCategory(category)
+end
+function Steps.OptionsPanel_OKAY()
+	-- Data was recorded, clear the temp
+	Steps.oldValues = nil
+end
+function Steps.OptionsPanel_Cancel()
+	-- reset to temp and update the UI
+	if Steps.oldValues then
+		for key,val in pairs(Steps.oldValues) do
+			Steps_options[key] = val
+		end
+	end
+	Steps.oldValues = nil
+end
+function Steps.OptionPanel_KeepOriginalValue( option )
+	if Steps.oldValues then
+		Steps.oldValues[option] = Steps.oldValues[option] or Steps_options[option]
+	else
+		Steps.oldValues={[option] = Steps_options[option]}
+	end
+end
+function Steps.OptionsPanel_CheckButton_OnLoad( self, option, text )
+	--FB.Print("CheckButton_OnLoad( "..option..", "..text.." ) -> "..(FB_options[option] and "checked" or "nil"));
+	getglobal(self:GetName().."Text"):SetText(text)
+	self:SetChecked(Steps_options[option])
+end
+-- OnClick for checkbuttons
+function Steps.OptionsPanel_CheckButton_OnClick( self, option )
+	Steps.OptionPanel_KeepOriginalValue( option )
+	Steps_options[option] = self:GetChecked()
+end
+
+Steps.commandList["options"] = {
+	["func"] = function() Settings.OpenToCategory( StepsOptionsFrame.category:GetID() ) end,
+	["help"] = {"", "Open the options panel"},
+}

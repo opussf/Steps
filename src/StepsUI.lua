ba0ebaa90c0ccef9a164b82b5b83fe40048af5b8
opussf <github@zz9-za.com>
2025-04-23 14:28:01 -0700

    Put debug info behind debug flag, and 2Week function.
    In order to keep engineers and scientists cognizant of the importance of progress, load them down with forms, multiple reports, and frequent meetings.

--- src/StepsUI.lua
+++ src/StepsUI.lua
@@ -48,7 +48,7 @@ function Steps.ShowWeek()
 			barData[dayStr][1] = steps
 			outStr = outStr .. " "..steps.."/"..barMax
 		end
-		Steps.Print( outStr )
+		if Steps.debug then Steps.Print( outStr ) end
 	end
 	for dayBack = 0, 6 do
 		dayStr = date( "%Y%m%d", time() - (dayBack*86400) )
@@ -73,18 +73,50 @@ function Steps.Show2Week()
 		return
 	end
 
+	local barMax = 0
+	local barData = {}    -- [%Y%m%d] = {mine, total}
+
+	for dayBack = 0, 13 do
+		dayStr = date( "%Y%m%d", time() - (dayBack*86400) )
+		outStr = "Looking at: "..dayStr.." dayBack:"..dayBack
+		barData[dayStr] = {0,0}
+		for r,ra in pairs( Steps_data ) do
+			for n, na in pairs( ra ) do
+				if na[dayStr] then
+					barData[dayStr][2] = barData[dayStr][2] + na[dayStr].steps
+					barMax = max( barMax, barData[dayStr][2] )
+				end
+			end
+		end
+		if Steps.mine[dayStr] then
+			local steps = math.floor( Steps.mine[dayStr].steps )
+			barMax = max( barMax, steps )
+			barData[dayStr][1] = steps
+			outStr = outStr .. " "..steps.."/"..barMax
+		end
+		if Steps.debug then Steps.Print( outStr ) end
+	end
+	for dayBack = 0, 13 do
+		dayStr = date( "%Y%m%d", time() - (dayBack*86400) )
+		local steps = math.floor( barData[dayStr][1] )
+		Steps.MineBars[14-dayBack]:SetMinMaxValues( 0, barMax )
+		Steps.MineBars[14-dayBack]:SetValue( math.floor( barData[dayStr][1] ) )
+		Steps.HistBars[14-dayBack]:SetMinMaxValues( 0, barMax )
+		Steps.HistBars[14-dayBack]:SetValue( math.floor( barData[dayStr][2] ) )
+	end
+
+
 end
 function Steps.AssureBars( barsNeeded, width )
 	width = width or 30
 	local count = #Steps.MineBars
 	if( not InCombatLockdown() and barsNeeded > count  ) then
-		Steps.Print( "I need "..barsNeeded.."/"..count.." bars." )
+		if Steps.debug then Steps.Print( "I need "..barsNeeded.."/"..count.." bars." ) end
 		for i = count+1, barsNeeded do
-			Steps.Print( "Creating bar# "..i )
+			if Steps.debug then Steps.Print( "Creating bar# "..i ) end
 			local newBar = CreateFrame( "StatusBar", "Steps_MineBar"..i, StepsUI_BarFrame, "Steps_TrendBarTemplate" )  --template can be last parameter
 			newBar:SetFrameStrata( "MEDIUM" )
 			newBar:SetStatusBarColor( unpack( Steps.stepsColor ) )  -- Should be the gold color
-			newBar:SetWidth( width )
 			if( i == 1 ) then
 				newBar:SetPoint( "TOPLEFT", "StepsUI_BarFrame", "TOPLEFT" )
 			else
@@ -93,7 +125,6 @@ function Steps.AssureBars( barsNeeded, width )
 			Steps.MineBars[i] = newBar
 			newBar = CreateFrame( "StatusBar", "Steps_HistBar"..i, StepsUI_BarFrame, "Steps_TrendBarTemplate" )
 			newBar:SetFrameStrata( "LOW" )
-			newBar:SetWidth( width )
 			if( i == 1 ) then
 				newBar:SetPoint( "TOPLEFT", "StepsUI_BarFrame", "TOPLEFT" )
 			else
@@ -104,7 +135,9 @@ function Steps.AssureBars( barsNeeded, width )
 	end
 	for i = 1, barsNeeded do
 		Steps.MineBars[i]:Show()
+		Steps.MineBars[i]:SetWidth( width )
 		Steps.HistBars[i]:Show()
+		Steps.HistBars[i]:SetWidth( width )
 	end
 	for i = barsNeeded+1, count do
 		Steps.MineBars[i]:Hide()
@@ -115,10 +148,10 @@ end
 function Steps.AssureXAxis( needed, labels )
 	local count = #Steps.XAxis
 	if( not InCombatLockdown() and needed > count ) then
-		Steps.Print( "I need "..needed.."/"..count.." XAxis." )
+		if Steps.debug then Steps.Print( "I need "..needed.."/"..count.." XAxis." ) end
 		local dayIndex = 1
 		for i = count+1, needed do
-			Steps.Print( "Creating XAxis# "..i )
+			if Steps.debug then Steps.Print( "Creating XAxis# "..i ) end
 			local newButton = CreateFrame( "Button", "Steps_XAxis"..i, StepsUI_Frame, "Steps_XAxisButtonTemplate" )
 			newButton:SetSize( 30, 20 )
 			if( i == 1 ) then
@@ -130,6 +163,13 @@ function Steps.AssureXAxis( needed, labels )
 			dayIndex = dayIndex + 1
 			Steps.XAxis[i] = newButton
 		end
+	end
+	for i = 1, needed do
+		Steps.XAxis[i]:SetText( labels[i] )
+		Steps.XAxis[i]:Show()
+	end
+	for i = needed+1, count do
+		Steps.XAxis[i]:Hide()
 	end
 	return max( count, needed )
 end

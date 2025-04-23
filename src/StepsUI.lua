08db818f953b27bd47d0eec575f157644ffae560
opussf <github@zz9-za.com>
2025-04-22 17:04:25 -0700

    Dual Bars
    The gods envy us, because we are mortal. Any moment might be our last. Everything is more beautiful because we are doomed.

--- src/StepsUI.lua
+++ src/StepsUI.lua
@@ -1,6 +1,7 @@
 STEPS_SLUG, Steps = ...
 
-Steps.UIBars = {}
+Steps.MineBars = {}
+Steps.HistBars = {}
 
 function Steps.ShowTrend()
 	if StepsUI_Frame:IsVisible() then
@@ -15,38 +16,60 @@ function Steps.ShowTrend()
 	end
 
 	local barMax = 0
+	local barData = {}    -- [%Y%m%d] = {mine, total}
+
 	for dayBack=0, 6 do
 		dayStr = date( "%Y%m%d", time() - (dayBack*86400) )
 		outStr = "Looking at: "..dayStr.." dayBack:"..dayBack
+		barData[dayStr] = {0,0}
+		for r,ra in pairs( Steps_data ) do
+			for n, na in pairs( ra ) do
+				if na[dayStr] then
+					barData[dayStr][2] = barData[dayStr][2] + na[dayStr].steps
+					barMax = max( barMax, barData[dayStr][2] )
+				end
+			end
+		end
 		if Steps.mine[dayStr] then
 			local steps = math.floor( Steps.mine[dayStr].steps )
 			barMax = max( barMax, steps )
+			barData[dayStr][1] = steps
 			outStr = outStr .. " "..steps.."/"..barMax
 		end
 		Steps.Print( outStr )
 	end
 	for dayBack = 0, 6 do
 		dayStr = date( "%Y%m%d", time() - (dayBack*86400) )
-		local steps = Steps.mine[dayStr] and math.floor( Steps.mine[dayStr].steps ) or 0
-		Steps.UIBars[7-dayBack]:SetMinMaxValues( 0, barMax )
-		Steps.UIBars[7-dayBack]:SetValue( steps )
+		local steps = math.floor( barData[dayStr][1] )
+		Steps.MineBars[7-dayBack]:SetMinMaxValues( 0, barMax )
+		Steps.MineBars[7-dayBack]:SetValue( math.floor( barData[dayStr][1] ) )
+		Steps.HistBars[7-dayBack]:SetMinMaxValues( 0, barMax )
+		Steps.HistBars[7-dayBack]:SetValue( math.floor( barData[dayStr][2] ) )
 	end
 end
 
 function Steps.AssureBars( barsNeeded )
-	local count = #Steps.UIBars
+	local count = #Steps.MineBars
 	if( not InCombatLockdown() and barsNeeded > count  ) then
 		Steps.Print( "I need "..barsNeeded.."/"..count.." bars." )
 		for i = count+1, barsNeeded do
 			Steps.Print( "Creating bar# "..i )
-			local newBar = CreateFrame( "StatusBar", "Steps_Bar"..i, StepsUI_Frame, "Steps_TrendBarTemplate" )  --template can be last parameter
+			local newBar = CreateFrame( "StatusBar", "Steps_MineBar"..i, StepsUI_Frame, "Steps_TrendBarTemplate" )  --template can be last parameter
 			newBar:SetFrameStrata( "LOW" )
 			if( i == 1 ) then
 				newBar:SetPoint( "TOPLEFT", "StepsUI_Frame", "TOPLEFT" )
 			else
-				newBar:SetPoint( "TOPLEFT", Steps.UIBars[i-1], "TOPRIGHT" )
+				newBar:SetPoint( "TOPLEFT", Steps.MineBars[i-1], "TOPRIGHT" )
 			end
-			Steps.UIBars[i] = newBar
+			Steps.MineBars[i] = newBar
+			newBar = CreateFrame( "StatusBar", "Steps_HistBar"..i, StepsUI_Frame, "Steps_TrendBarTemplate" )
+			newBar:SetFrameStrata( "LOW" )
+			if( i == 1 ) then
+				newBar:SetPoint( "TOPLEFT", "StepsUI_Frame", "TOPLEFT" )
+			else
+				newBar:SetPoint( "TOPLEFT", Steps.HistBars[i-1], "TOPRIGHT" )
+			end
+			Stes.HistBars[i] = newBar
 		end
 	end
 	return max( count, barsNeeded )
